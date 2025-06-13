1018458c26aa5f9d2067e9b3b57654eae759e458
Arcesio Castaneda Medina <arcesio.castaneda.medina@itwm.fraunhofer.de>
2025-06-13 13:01:16 +0200

    Add MPS to circuit method

--- src/qiskit_quimb/circuit.py
+++ src/qiskit_quimb/circuit.py
@@ -13,7 +13,7 @@ import quimb.tensor
 from qiskit.circuit import QuantumCircuit
 
 from qiskit_quimb.gate import quimb_gates
-
+from mps_to_circuit import mps_to_circuit
 
 def quimb_circuit(
     circuit: QuantumCircuit,
@@ -36,3 +36,29 @@ def quimb_circuit(
     return quimb_circuit_class.from_gates(
         quimb_gates(circuit), N=circuit.num_qubits, **kwargs
     )
+
+def qiskit_circuit(
+    circuit: quimb.tensor.Circuit,
+    quimb_circuit_class: type[quimb.tensor.Circuit] = quimb.tensor.Circuit,
+    **kwargs,
+) -> QuantumCircuit:
+    """Convert a Qiskit circuit to a quimb circuit.
+
+    The quimb circuit is constructed using the ``from_gates`` method of the quimb
+    circuit class (``quimb.tensor.Circuit`` by default), passing along the keyword
+    arguments from ``kwargs``.
+
+    Args:
+        circuit: The Qiskit circuit.
+        quimb_circuit_class: The ``quimb.tensor.Circuit`` subclass to use.
+
+    Returns:
+        The quimb circuit.
+    """
+
+    mps_arrays = list(circuit.psi.arrays)
+
+    # Convert the list of quimb MPS tensors to a Qiskit circuit
+    qiskit_circ = mps_to_circuit(mps_arrays, method="exact")
+
+    return qiskit_circ
